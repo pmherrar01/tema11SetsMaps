a9fe3a74f9ae81fb9d7aeec0cc80355766b47a3f
pmherrar01 <120454320+pmherrar01@users.noreply.github.com>
2024-06-10 20:53:43 +0200

    Ejercicio 2 por terminar

--- Ejercicio2/src/Ejercicio2.java
+++ Ejercicio2/src/Ejercicio2.java
@@ -1,12 +1,12 @@
 import java.util.Scanner;
-import java.util.HashMap;
 import java.util.Map;
+import java.util.HashMap;
 
 public class Ejercicio2 {
 
 	public static void main(String[] args) {
-		Map <Integer, Producto> mapa = new HashMap<>();
-
+		
+		Map<Integer,Producto> mapa = new HashMap<>();
 	}
 
 }
--- Ejercicio2/src/Producto.java
+++ Ejercicio2/src/Producto.java
@@ -10,30 +10,37 @@ public class Producto {
 		this.precio = 0;
 		this.stock = 0;
 	}
-
+	
 	public Producto(String nombreProducto, float precio, int stock) {
 		this.nombreProducto = nombreProducto;
 		this.precio = precio;
-		stock = stock;
+		this.stock = stock;
 	}
+
 	public String getNombreProducto() {
 		return nombreProducto;
 	}
+
 	public void setNombreProducto(String nombreProducto) {
 		this.nombreProducto = nombreProducto;
 	}
+
 	public float getPrecio() {
 		return precio;
 	}
+
 	public void setPrecio(float precio) {
 		this.precio = precio;
 	}
+
 	public int getStock() {
 		return stock;
 	}
+
 	public void setStock(int stock) {
-		stock = stock;
+		this.stock = stock;
 	}
+	
 	
 	
 }
